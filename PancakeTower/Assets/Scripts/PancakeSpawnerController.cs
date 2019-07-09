71ef378c7a4a1c2918bd8b9c61ca0a50ed448c11
Steffi <stefanie.m.kloss@t-online.de>
2019-07-09 02:16:00 +0200

    Basic spawning of pancakes works

--- PancakeTower/Assets/Scripts/PancakeSpawnerController.cs
+++ PancakeTower/Assets/Scripts/PancakeSpawnerController.cs
@@ -7,25 +7,34 @@ public class PancakeSpawnerController : MonoBehaviour
 {
     public GameObject pancakePrefab;
     private Vector2 screenBounds;
+    private float timer;
+    public float pancakeSpawnVelocity;
     // Start is called before the first frame update
     void Start()
     {
+        timer = 0.0f;
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
-        SpawnPancakes();
+        SpawnPancake();
     }
 
-    void SpawnPancakes()
+    void SpawnPancake()
     {
         GameObject pancakeInstance = Instantiate(pancakePrefab) as GameObject;
+        pancakeInstance.GetComponent<Rigidbody2D>().gravityScale = 1;
         int random = new System.Random().Next(15, 20);
         pancakeInstance.GetComponent<Transform>().localScale = new Vector3(random * 0.01f, -0.15f, 0);
         float spawnX = UnityEngine.Random.Range(-screenBounds.x, screenBounds.x);
-        pancakeInstance.transform.position = new Vector2(spawnX, 10);
+        pancakeInstance.transform.position = new Vector2(spawnX, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        timer += Time.deltaTime;
+        if (timer > pancakeSpawnVelocity)
+        {
+            this.SpawnPancake();
+            timer = 0;
+        }
     }
 }
