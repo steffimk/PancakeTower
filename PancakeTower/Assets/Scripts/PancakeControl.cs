bbc205aa5eb6db2a8574e5c843ced8c9db04ddf5
Felix <felix.sommer@adesso.de>
2019-07-10 12:22:00 +0200

    pancake pile on plate

--- PancakeTower/Assets/Scripts/PancakeControl.cs
+++ PancakeTower/Assets/Scripts/PancakeControl.cs
@@ -6,22 +6,46 @@ public class PancakeControl : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private CapsuleCollider2D collider;
+    private Transform trans;
+    private bool onPlate = false;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         collider = GetComponent<CapsuleCollider2D>();
+        trans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //trans.SetPositionAndRotation(new Vector3(trans.parent.position.x, trans.position.y, 0), 0.0f) = trans.parent.position.x;
+
+        if (onPlate)
+        {
+            trans.position = new Vector3(trans.parent.position.x, trans.position.y, 0);
+
+        }
+
     }
+
+    
 
     void OnTriggerEnter2D(Collider2D collider2D)
     {
-        rb2d.gravityScale = 0;
-        rb2d.velocity = new Vector2(0.0f, 0.0f);
-        Debug.Log("Baaaaam");
+
+        if (collider2D.gameObject.name != "WallLeft" && collider2D.gameObject.name != "WallRight")
+        {
+            rb2d.gravityScale = 0;
+            rb2d.velocity = new Vector2(0.0f, 0.0f);
+            onPlate = true;
+
+            GetComponent<Transform>().SetParent(collider2D.transform);
+            Debug.Log("Baaaaam");
+        }
+      
+
     }
 }
